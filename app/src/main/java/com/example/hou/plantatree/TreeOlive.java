bbc61c2bccf8469888f5dec1aea530644007e7a2
Taowww <42598591+Taowww@users.noreply.github.com>
2018-09-24 20:50:12 +1200

    Add files via upload

--- app/src/main/java/com/example/hou/plantatree/TreeOlive.java
+++ app/src/main/java/com/example/hou/plantatree/TreeOlive.java
@@ -3,19 +3,30 @@ package com.example.hou.plantatree;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
 public class TreeOlive extends AppCompatActivity {
+    private EditText amount;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_olive);
         setTitle("Olive");
+
+        amount=findViewById(R.id.amount_olive);
+        add=findViewById(R.id.add_cart_olive);
+
+        amount.addTextChangedListener(inputTextWatch);
+
 
         ImageButton back_olive=(ImageButton)findViewById(R.id.back_button_olive);
         back_olive.setOnClickListener(new View.OnClickListener() {
@@ -26,13 +37,13 @@ public class TreeOlive extends AppCompatActivity {
             }
         });
 
+
     }
 
     public void clickAdd(View view){
-        EditText number=(EditText) findViewById(R.id.quantive_olive);
         String name="Olive";
         String price="12";
-        String qty=number.getText().toString();
+        String qty=amount.getText().toString();
 
         Intent intent=new Intent(TreeOlive.this,Cart.class);
 
@@ -42,5 +53,25 @@ public class TreeOlive extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    private TextWatcher inputTextWatch=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String amountInput=amount.getText().toString();
+
+            add.setEnabled(!amountInput.isEmpty()&&!amountInput.contains("-"));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 
 }
