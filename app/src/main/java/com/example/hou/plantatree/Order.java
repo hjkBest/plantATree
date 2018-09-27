e9d5f9747cd9e53597868ab33ed85d9c57750add
Logan Betty <31017872+loganbetty@users.noreply.github.com>
2018-09-28 11:14:46 +1200

    Add files via upload

---
+++ app/src/main/java/com/example/hou/plantatree/Order.java
@@ -0,0 +1,16 @@
+package com.example.hou.plantatree;
+
+public class Order
+{
+    String name;
+    String quantity;
+    String price;
+    String height;
+    String age;
+
+
+    public Order(String name, String quantity,String price,
+                 String height,String age){
+
+    }
+}
