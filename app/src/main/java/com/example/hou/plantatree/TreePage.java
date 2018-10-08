e75305eaf63c468010d63ccaaa6fd1742f0922cf
Bob Chang <bobdadesigner@gmail.com>
2018-10-08 14:12:09 +1300

    login updated

--- app/src/main/java/com/example/hou/plantatree/TreePage.java
+++ app/src/main/java/com/example/hou/plantatree/TreePage.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class TreePage extends AppCompatActivity{
     
